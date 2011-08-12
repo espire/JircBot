14ca83df629abca7c4bc36b4bc6671e976933f01
Eli Spiro <elispiro@gmail.com>
2011-08-11 21:53:21 -0400

    improved style, more verbose logging, externalized vote

--- Snark.java
+++ Snark.java
@@ -4,40 +4,30 @@ import java.util.Random;
 
 class Snark {
 	
-	String nick;
-	
 	// Random number generator
 	Random randomGenerator = new Random();
 	
-	ArrayList<Element> list;
-	
-	public Snark(String nick) {
-		XmlReader snarkXml = new XmlReader("snark.xml");
-		list = snarkXml.toList();
-		this.nick = nick;
+	public Vote() {
+		System.err.println("Loading Vote module:");
 		
-		for(Element e : list) {
-			System.out.println("Snark enabled: " + e.getAttribute("in") + " -> " + e.getAttribute("out") + ", 1 in " + e.getAttribute("chance") + " times.");
-		}
+		System.err.println();
 	}
 	
 	public String feed(Message message) {
 		
-		if(message.type.equals("PRIVMSG") && !message.author.equals(nick)) { // is that second part necessary?
-			ArrayList<String> responses = new ArrayList<String>();
-			String content = message.content.toLowerCase();
-			
-			for(Element e : list) {
-				if (content.contains(e.getAttribute("in"))) {
-					if (randomGenerator.nextInt(Integer.parseInt(e.getAttribute("chance"))) == 0) {
-						responses.add(e.getAttribute("out"));
-					}
+		ArrayList<String> responses = new ArrayList<String>();
+		String content = message.content.toLowerCase();
+		
+		for (Element e : list) {
+			if (content.contains(e.getAttribute("in"))) {
+				if (randomGenerator.nextInt(Integer.parseInt(e.getAttribute("chance"))) == 0) {
+					responses.add(e.getAttribute("out"));
 				}
 			}
-			
-			if (!responses.isEmpty()) {
-				return responses.get(randomGenerator.nextInt(responses.size()));
-			}
+		}
+
+		if (!responses.isEmpty()) {
+			return responses.get(randomGenerator.nextInt(responses.size()));
 		}
 		
 		return "";
