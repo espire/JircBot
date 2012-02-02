d6770672bbfccf5a877c5911d3ff64bb1be077ae
Eli Spiro <elispiro@gmail.com>
2012-02-01 21:38:00 -0800

    Context-aware snarks and no crash on /nick

--- Message.java
+++ Message.java
@@ -28,55 +28,67 @@ public class Message {
 	public Message(String line) {
 		raw = line;
 		
-		// set the timestamp
-		cal = Calendar.getInstance();
-		sdf = new SimpleDateFormat(timeFormat);
-		timeStamp = sdf.format(cal.getTime());
+		try {
 		
-		// skip the leading :, author is up to the first !
-		author = line.substring(1, line.indexOf('!'));
-		
-		// remove up to and including the !
-		line = line.substring(line.indexOf('!') + 1);
-		
-		// username is up to the space
-		user = line.substring(0, line.indexOf(' '));
-		
-		// remove up to and including the space
-		line = line.substring(line.indexOf(' ') + 1);
-		
-		// type is up to the next space
-		type = line.substring(0, line.indexOf(' '));
-		
-		// remove up to and including the space
-		line = line.substring(line.indexOf(' ') + 1);
-		
-		if (type.equals("JOIN")) {
-			// content (the channel joined) is from the leading : on
-			content = line.substring(1);
-		}
-		else if (type.equals("PART")) {
-			// recipient (the channel parted) is the remainder of the message
-			recipient = line;
-		}
-		else {
-			// recipient is up to the space
-			recipient = line.substring(0, line.indexOf(' '));
+			// set the timestamp
+			cal = Calendar.getInstance();
+			sdf = new SimpleDateFormat(timeFormat);
+			timeStamp = sdf.format(cal.getTime());
 			
-			// remove up to and including the space, then the :
-			line = line.substring(line.indexOf(' ') + 2);
+			// skip the leading :, author is up to the first !
+			author = line.substring(1, line.indexOf('!'));
 			
-			if (line.startsWith(((char)1) + "ACTION")) {
-				type = "ACTION";
-				
-				// remove up to and including the first space
-				line = line.substring(line.indexOf(' ') + 1);
+			// remove up to and including the !
+			line = line.substring(line.indexOf('!') + 1);
+			
+			// username is up to the space
+			user = line.substring(0, line.indexOf(' '));
+			
+			// remove up to and including the space
+			line = line.substring(line.indexOf(' ') + 1);
+			
+			// type is up to the next space
+			type = line.substring(0, line.indexOf(' '));
+			
+			// remove up to and including the space
+			line = line.substring(line.indexOf(' ') + 1);
+			
+			if (type.equals("JOIN")) {
+				// content (the channel joined) is from the leading : on
+				content = line.substring(1);
+			}
+			else if (type.equals("PART")) {
+				// recipient (the channel parted) is the remainder of the message
+				recipient = line;
+			}
+			else if (type.equals("NICK")) {
+				// new nick is from the leading : on
+				content = line.substring(1);
 			}
 			
-			// content (the channel joined) is from the leading : on
-			content = line;
+			else {
+				// recipient is up to the space
+				recipient = line.substring(0, line.indexOf(' '));
+								
+				// remove up to and including the space, then the :
+				line = line.substring(line.indexOf(' ') + 2);
+				
+				if (line.startsWith(((char)1) + "ACTION")) {
+					type = "ACTION";
+					
+					// remove up to and including the first space
+					line = line.substring(line.indexOf(' ') + 1);
+				}
+				
+				// content (the channel joined) is from the leading : on
+				content = line;
+			}
+		} catch (Exception e) {
+			// if the message crashes us, print it out for debugging!
+			
+			System.err.println("Failed to parse incoming message. The message was:");
+			System.err.println(raw);
 		}
-		
 	}
 	
 	public String toString() {
@@ -87,13 +99,16 @@ public class Message {
 			return "[" + timeStamp + "] " + author + " has left " + recipient + ".";
 		}
 		else if (type.equals("ACTION")) {
-			return "[" + timeStamp + "] *" + author + " " + content;
+			return "[" + timeStamp + "] * " + author + " " + content;
 		}
 		else if (type.equals("NOTICE")) {
 			return "[" + timeStamp + "] NOTICE <" + author + "> " + content;
 		}		
 		else if (type.equals("PRIVMSG")) {
 			return "[" + timeStamp + "] <" + author + "> " + content;
+		}
+		else if (type.equals("NICK")) {
+			return "[" + timeStamp + "] * " + author + " is now known as " + content +".";
 		}
 		else {
 			return "I don't know what to do with this message:\n" + raw;
--- Snark.java
+++ Snark.java
@@ -17,6 +17,7 @@ class Snark {
 	// Random number generator
 	Random randomGenerator = new Random();
 	
+	// the list of words to snark
 	ArrayList<Element> list;
 	
 	public Snark() {
@@ -25,7 +26,12 @@ class Snark {
 		list = snarkXml.toList();
 		
 		for (Element e : list) {
-			System.out.println("Snark: " + e.getAttribute("in") + " -> " + e.getAttribute("out") + ", 1 out of " + e.getAttribute("chance") + " times.");
+			System.out.print("Snark: " + e.getAttribute("in") + " -> " + e.getAttribute("out") + ", 1 out of " + e.getAttribute("chance") + " times");
+			if (e.getAttribute("author").equals("*")) {
+				System.out.println(" for anyone.");
+			} else {
+				System.out.println(" for " + e.getAttribute("author") + ".");
+			}
 		}
 		
 		System.err.println();
@@ -35,15 +41,19 @@ class Snark {
 		
 		ArrayList<String> responses = new ArrayList<String>();
 		String content = message.content.toLowerCase();
+		String author = message.author.toLowerCase();
 		
+		// go through the list of snarks and give each one a chance to snark
 		for (Element e : list) {
-			if (content.contains(e.getAttribute("in"))) {
+			if (content.contains(e.getAttribute("in"))
+				&& (e.getAttribute("author").equals(author) || e.getAttribute("author").equals("*"))) {
 				if (randomGenerator.nextInt(Integer.parseInt(e.getAttribute("chance"))) == 0) {
 					responses.add(e.getAttribute("out"));
 				}
 			}
 		}
 
+		// if more than one snark responded, choose a random one
 		if (!responses.isEmpty()) {
 			return responses.get(randomGenerator.nextInt(responses.size()));
 		}
